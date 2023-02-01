19c66e982986080f7774f1f3e5d11f328c77da26
Juan Garcia <juanmgarcia098@gmail.com>
2023-01-31 22:50:45 -0400

    Stub up Resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,12 @@
 function Resume() {
   return (
+    <>
       <h1>Resume</h1>
+      <embed src="Resume.pdf" type="application/pdf" id="resume" />
+      <a href="Resume.pdf" download={Resume.pdf}>
+        <button>Download Resume</button>
+      </a>
+    </>
   )
 }
 
