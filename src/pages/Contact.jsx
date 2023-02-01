b455745770c5c3a52947d55d8a47bf56687dfdac
Juan Garcia <juanmgarcia098@gmail.com>
2023-01-31 22:36:50 -0400

    Stub up Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,10 @@
 function Contact() {
   return (
+    <>
       <h1>Contact</h1>
+      <h1><a href="https://github.com/juanm98">GitHub</a></h1>
+      <h1><a href="https://www.linkedin.com/in/juan-grullon/">LinkedIn</a></h1>
+    </>
   )
 }
 
