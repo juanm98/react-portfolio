6b21f53f4d86822eb4b20fe930910a1115f169c8
Juan Garcia <juanmgarcia098@gmail.com>
2023-02-02 02:57:05 -0400

    Add email

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,7 @@ function Contact() {
   return (
     <>
       <h1>Contact</h1>
+      <p>You can contact me through my <a href="juanmgarcia098@gmail.com">email</a></p>
       <h1><a href="https://github.com/juanm98">GitHub</a></h1>
       <h1><a href="https://www.linkedin.com/in/juan-grullon/">LinkedIn</a></h1>
     </>
