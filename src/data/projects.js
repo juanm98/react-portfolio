500eff880fce9b1a7df08ef29898d5788d7ded05
Juan Garcia <juanmgarcia098@gmail.com>
2023-02-02 02:35:03 -0400

    Fix Images

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,14 +2,14 @@ const projectsData = [
   {
     title: "Connect-Force",
     description: "Connect Force is a re-creation of Connect Four! Both players begin with 21 identical pieces, and the first player to achieve a line of four connected pieces wins the game. If all 42 tokens are played the game is drawn.",
-    image: <img src="../assets/Connect-Force.png" alt="connect-force"/>,
+    image: '/images/Connect-Force.png',
     repositoryLink: "https://github.com/juanm98/Connect-Force",
     deployementLink: "https://juans-connect-four.netlify.app/"
   },
   {
     title: "ShopSmart",
     description: "ShopSmart is a shopping list app that helps you stay organized and save time. Just log in with your Google account and start making shopping lists!",
-    image: <img src="../assets/ShoppingList.png" alt="shopping-list"/>,
+    image: '/images/ShoppingList.png',
     repositoryLink: "https://github.com/juanm98/Shopping-List",
     deployementLink: "https://shoppinglistfly.fly.dev/"
   }
