6d12658969a31de056dd382f3be62142b3b6231b
Juan Garcia <juanmgarcia098@gmail.com>
2023-03-08 13:17:28 -0400

    Align items

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 function Home() {
   return (
     <>
+    <div id="starter-info">
       <h1>Juan Garcia</h1>
       <h1>Software Engineer</h1>
       <img id="home" src="mypic.jpg" alt="placeholder" />
+      </div>
     </>
   )
 }
