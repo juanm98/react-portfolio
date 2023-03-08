f33ae77b15351e7da7f4917181ddf482ee3f6027
Juan Garcia <juanmgarcia098@gmail.com>
2023-03-07 21:25:56 -0400

    Add projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,6 +12,20 @@ const projectsData = [
     image: '/images/ShoppingList.png',
     repositoryLink: "https://github.com/juanm98/Shopping-List",
     deployementLink: "https://shoppinglistfly.fly.dev/"
+  },
+  {
+    title: "Restful Rooms!",
+    description: "Restful Rooms is a platform for finding short-term accommodations for programmers who are traveling for work. Like Airbnb, it allows homeowners to rent out their spare rooms or entire homes to guests. ",
+    image: '/images/restful-Rooms.png',
+    repositoryLink: "https://github.com/izabela2279/restful-rooms-front-end",
+    deployementLink: "https://restful-rooms.netlify.app/"
+  },
+  {
+    title: "Flitter!",
+    description: "Flitter Flitter is a social media application that allows users to share their thoughts, opinions, and updates with their followers. ",
+    image: '/images/flitter4.png',
+    repositoryLink: "https://github.com/juanm98/flitter-front-end",
+    deployementLink: "https://flitter-app.netlify.app/"
   }
 ]
 
